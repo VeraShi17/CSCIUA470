f3b52af00debfef3cfb92b2ef8c9cff044ee4ccc
Jiarui <104609670+VeraShi17@users.noreply.github.com>
2023-11-30 15:33:20 -0500

    Update RecruiterSignupGUI.java

--- RecruiterSignupGUI.java
+++ RecruiterSignupGUI.java
@@ -1,18 +1,8 @@
-import java.awt.EventQueue;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.SwingConstants;
-import java.awt.Font;
+import java.awt.*;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.awt.event.ActionEvent;
 
 public class RecruiterSignupGUI extends JFrame {
@@ -21,7 +11,11 @@ public class RecruiterSignupGUI extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtUsername;
 	private JTextField txtPassword;
-	private String username;
+	private JLabel lblSignup;
+	private JLabel lblUsername;
+	private JLabel lblPassword;
+	private JButton btnSign;
+	private JButton btnCancel;
 	
 	public RecruiterSignupGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,17 +26,17 @@ public class RecruiterSignupGUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblSignup = new JLabel("Recruiter Sign Up");
+		lblSignup = new JLabel("Recruiter Sign Up");
 		lblSignup.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
 		lblSignup.setBounds(134, 6, 160, 26);
 		contentPane.add(lblSignup);
 		
-		JLabel lblUsername = new JLabel("Username");
+		lblUsername = new JLabel("Username");
 		lblUsername.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		lblUsername.setBounds(80, 81, 84, 16);
 		contentPane.add(lblUsername);
 		
-		JLabel lblPassword = new JLabel("Password");
+		lblPassword = new JLabel("Password");
 		lblPassword.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		lblPassword.setBounds(80, 141, 84, 16);
 		contentPane.add(lblPassword);
@@ -59,7 +53,7 @@ public class RecruiterSignupGUI extends JFrame {
 		contentPane.add(txtPassword);
 		txtPassword.setColumns(10);
 		
-		JButton btnSign = new JButton("Sign up");
+		btnSign = new JButton("Sign up");
 		btnSign.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// verify whether 1. username exists 2. txt empty
@@ -108,7 +102,7 @@ public class RecruiterSignupGUI extends JFrame {
 		btnSign.setBounds(80, 201, 117, 29);
 		contentPane.add(btnSign);
 		
-		JButton btnCancel = new JButton("Cancel");
+		btnCancel = new JButton("Cancel");
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
