c0f3f3fd09b27513de7a076d255aab81779f86a0
Jiarui <104609670+VeraShi17@users.noreply.github.com>
2023-12-09 19:14:26 -0500

    Update ViewJobDetailsGUI.java

--- ViewJobDetailsGUI.java
+++ ViewJobDetailsGUI.java
@@ -17,6 +17,16 @@ public class ViewJobDetailsGUI extends JFrame {
     private JLabel lblRecruiterCompanyName;
     private JLabel lblRecruiterCompanyDescription;
     private JButton btnCancel;
+    private DatabaseConnect conn;
+    private ResultSet resultSet;
+    private String jobDescription;
+    private String location;
+    private String educationRequirements;
+    private String recruiterName;
+    private String recruiterPhone;
+    private String recruiterEmail;
+    private String recruiterCompanyName;
+    private String recruiterCompanyDescription;
 
     public ViewJobDetailsGUI(String recruiterUsername, String jobTitle) {
         setTitle("Job Details");
@@ -59,63 +69,67 @@ public class ViewJobDetailsGUI extends JFrame {
         populateJobDetails(recruiterUsername, jobTitle);
     }
 
-    private void populateJobDetails(String recruiterUsername, String jobTitle) {
-        try (Connection connection = DatabaseConnect.connect()) {
-            String selectJobDetailsQuery = "SELECT * FROM job WHERE job_title = ? and username = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(selectJobDetailsQuery)) {
-                preparedStatement.setString(1, jobTitle);
-                preparedStatement.setString(2, recruiterUsername);
-                ResultSet resultSet = preparedStatement.executeQuery();
+    
+    public void populateJobDetails(String recruiterUsername, String jobTitle) {
+        conn = new DatabaseConnect();
+        resultSet = conn.retrieveJobDetails(jobTitle, recruiterUsername);
 
-                if (resultSet.next()) {
-            
-                    String jobDescription = resultSet.getString("job_description");
-                    String location = resultSet.getString("location");
-                    String educationRequirements = resultSet.getString("education_requirements");
-                   
+        try {
+            if (resultSet.next()) {
+                jobDescription = resultSet.getString("job_description");
+                location = resultSet.getString("location");
+                educationRequirements = resultSet.getString("education_requirements");
 
-                    lblJobTitle.setText("Job Title: " + jobTitle);
-                    lblJobDescription.setText("Job Description: " + jobDescription);
-                    lblLocation.setText("Location: " + location);
-                    lblEducationRequirements.setText("Education Requirements: " + educationRequirements);
+                lblJobTitle.setText("Job Title: " + jobTitle);
+                lblJobDescription.setText("Job Description: " + jobDescription);
+                lblLocation.setText("Location: " + location);
+                lblEducationRequirements.setText("Education Requirements: " + educationRequirements);
 
-                    populateRecruiterDetails(recruiterUsername);
-                }
-
-                // Close the ResultSet
-                resultSet.close();
+                populateRecruiterDetails(recruiterUsername);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-    }
-
-    private void populateRecruiterDetails(String recruiterUsername) {
-        try (Connection connection = DatabaseConnect.connect()) {
-            String selectRecruiterDetailsQuery = "SELECT * FROM recruiter WHERE username = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(selectRecruiterDetailsQuery)) {
-                preparedStatement.setString(1, recruiterUsername);
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-                if (resultSet.next()) {
-                    String recruiterName = resultSet.getString("name");
-                    String recruiterPhone = resultSet.getString("phone");
-                    String recruiterEmail = resultSet.getString("email");
-                    String recruiterCompanyName = resultSet.getString("company_name");
-                    String recruiterCompanyDescription = resultSet.getString("company_description");
-
-                    lblRecruiterName.setText("Recruiter Name: " + recruiterName);
-                    lblRecruiterPhone.setText("Recruiter Phone: " + recruiterPhone);
-                    lblRecruiterEmail.setText("Recruiter Email: " + recruiterEmail);
-                    lblRecruiterCompanyName.setText("Recruiter Company Name: " + recruiterCompanyName);
-                    lblRecruiterCompanyDescription.setText("Recruiter Company Description: " + recruiterCompanyDescription);
+        } finally {
+            // Close the ResultSet, Statement, and Connection in the finally block
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
                 }
-
-                // Close the ResultSet
-                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+
         }
     }
+
+
+    public void populateRecruiterDetails(String recruiterUsername) {
+    	resultSet = conn.retrieveRecruiterDetails(recruiterUsername);
+    	try {
+			if (resultSet.next()) {
+			    recruiterName = resultSet.getString("name");
+			    recruiterPhone = resultSet.getString("phone");
+			    recruiterEmail = resultSet.getString("email");
+			    recruiterCompanyName = resultSet.getString("company_name");
+			    recruiterCompanyDescription = resultSet.getString("company_description");
+
+			    lblRecruiterName.setText("Recruiter Name: " + recruiterName);
+			    lblRecruiterPhone.setText("Recruiter Phone: " + recruiterPhone);
+			    lblRecruiterEmail.setText("Recruiter Email: " + recruiterEmail);
+			    lblRecruiterCompanyName.setText("Recruiter Company Name: " + recruiterCompanyName);
+			    lblRecruiterCompanyDescription.setText("Recruiter Company Description: " + recruiterCompanyDescription);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+        // Close the ResultSet
+        try {
+			resultSet.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+
+
 }
